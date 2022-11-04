869f610dc714b6240115f739c4ddc02a4ea1eb4f
yeansoo <raura98@naver.com>
2022-11-04 11:11:42 +0900

    commit

---
+++ Script-4.sql
@@ -0,0 +1,11 @@
+-- 수업용 프로젝트 계정 생성
+--CREATE USER project IDENTIFIED BY project1234;
+--
+-- 권한 부여
+--GRANT CONNECT, RESOURCE, CREATE VIEW TO project;
+--
+-- 객체 생성 공간 할당 
+--ALTER USER project DEFAULT TABLESPACE SYSTEM
+--QUOTA UNLIMITED ON SYSTEM;
+
+
